9c86fb605adcf0023e45f71cf64818eb2d8e11b7
Aksel Hjerpbakk <akselhj@stud.ntnu.no>
2018-04-25 02:33:41 +0200

    Update procmi.ps1

--- procmi.ps1
+++ procmi.ps1
@@ -4,7 +4,7 @@ Param([string[]]$param = $(throw "please send atleast one processID"))
     foreach ($procID in $param){
       $frame="******** Minne info om prosess med PID " + $procID +" ********`r`n"
       $vmsInfo = "Total bruk av virtuelt minne: " + (Get-Process -ID $procID).VirtualMemorySize/1MB + "MB`r`n"
-      $vsInfo = "Storrelse paa Working Set: " + (Get-Process -ID 4800).WorkingSet/1KB + "KB`r`n"
+      $vsInfo = "Storrelse paa Working Set: " + (Get-Process -ID $procID).WorkingSet/1KB + "KB`r`n"
       $fileContent = $frame + $vmsInfo + $vsInfo
       $path = "./" + ($procID) + (Get-Date -UFormat "--%Y%m%u--%H%M%S") + ".meminfo"
       $fileContent | Out-File $path
